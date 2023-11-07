e774197e73a1e6167053e9331e2ed3a7d781b07c
Dmitry <dmitry-maksimov@yandex.ru>
2023-11-07 19:46:16 +0300

    add allure reports

--- conftest.py
+++ conftest.py
@@ -1,13 +1,16 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
+import allure
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(4)
-    driver.maximize_window()
-    yield driver
+    with allure.step("Open and prepare browser"):
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(4)
+        driver.maximize_window()
+        yield driver
     
-    driver.quit()
+    with allure.step("Close browser"):
+        driver.quit()
--- test/auth_test.py
+++ test/auth_test.py
@@ -16,8 +16,9 @@ def test_auth(driver):
     main_page.open_menu()
     info = main_page.get_account_info()
     
-    with allure.step("Checking the right link"):
-        assert main_page.get_current_url().endswith("mdn782007/boards")
+    current_url = main_page.get_current_url()
+    with allure.step(f"Checking that {current_url} ends on mdn782007/boards"):
+        assert current_url.endswith("mdn782007/boards")
     with allure.step("Checking user info"):
         assert info[0] == username
         assert info[1] == email
