3210880c35718ad239e4fd89f13d8a92d99999c9
Dmitry <dmitry-maksimov@yandex.ru>
2023-11-07 19:13:23 +0300

    update gitignore

---
+++ test/auth_test.py
@@ -0,0 +1,18 @@
+from pages.AuthPage import AuthPage
+from pages.MainPage import MainPage
+import time
+
+
+def test_auth(driver):
+    auth_page = AuthPage(driver)
+    auth_page.go()
+    auth_page.login_as("salesakk@gmail.com", "111111Atlas")
+    time.sleep(5)
+    
+    main_page = MainPage(driver)
+    main_page.open_menu()
+    info = main_page.get_account_info()
+    
+    assert main_page.get_current_url().endswith("mdn782007/boards")
+    assert info[0] == "Dimitri"
+    assert info[1] == "salesakk@gmail.com"
