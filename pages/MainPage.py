00e536ab9a5cbc124bcd09b476a6f887b7847b7f
Dmitry <dmitry-maksimov@yandex.ru>
2023-11-25 20:19:38 +0300

    add allure steps

--- pages/MainPage.py
+++ pages/MainPage.py
@@ -21,11 +21,11 @@ class MainPage:
         
         return self.__driver.current_url
     
-    @allure.step("Open right side menu")
+    @allure.step("UI. Open right side menu")
     def open_menu(self):
         self.__driver.find_element(By.CSS_SELECTOR, "button[data-testid='header-member-menu-button']").click()
     
-    @allure.step("Get information about user")
+    @allure.step("UI. Get information about user")
     def get_account_info(self) -> list[str]:
         container = self.__driver.find_element(By.CSS_SELECTOR, "div[class=mJBO4dHZbrIG_0]")
         fields = container.find_elements(By.CSS_SELECTOR, "div")
@@ -33,22 +33,23 @@ class MainPage:
         email = fields[1].text
         return [name, email]
 
-    @allure.step("Open create form")
+    @allure.step("UI. Open create form")
     def open_create_form(self):
         self.__driver.find_element(By.CSS_SELECTOR, "button[data-testid=header-create-menu-button]").click()
     
-    @allure.step("Choose {number} element in list")
+    @allure.step("UI. Choose {number} element in list")
     def press_button_create_board(self, number=1):
         pop = self.__driver.find_element(By.CSS_SELECTOR, "section[data-testid=header-create-menu-popover]")
         menu_list = pop.find_elements(By.CSS_SELECTOR, "li")
         menu_list[number-1].click()
     
-    @allure.step("Select board {number} in workspace")
+    @allure.step("UI.Select board {number} in workspace")
     def select_board(self, number=0):
         pop = self.__driver.find_element(By.CSS_SELECTOR, "ul[class=boards-page-board-section-list]")
         menu_list = pop.find_elements(By.CSS_SELECTOR, "li")
         menu_list[number].click()
     
+    @allure.step("UI. Delete board")
     def delete_board(self):
         self.__driver.find_element(By.CSS_SELECTOR, "button.frrHNIWnTojsww.GDunJzzgFqQY_3.bxgKMAm3lq5BpA.HAVwIqCeMHpVKh.SEj5vUdI3VvxDc").click()
         WebDriverWait(self.__driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.board-menu-navigation-item-link.board-menu-navigation-item-link-v2.js-close-board"))).click()
@@ -56,11 +57,11 @@ class MainPage:
         WebDriverWait(self.__driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.Bp80TGmc9hQIdE.bxgKMAm3lq5BpA.V_9lMAQOdk_AYt.SEj5vUdI3VvxDc"))).click()
         WebDriverWait(self.__driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.a72r81xglmtLCW.bxgKMAm3lq5BpA.KpU415sFFvOzGZ.PnEv2xIWy3eSui.SEj5vUdI3VvxDc"))).click()
 
-    @allure.step("Set board name {board_name}")
+    @allure.step("UI. Set board name {board_name}")
     def fill_board_name(self, board_name: str):
         self.__driver.find_element(By.CSS_SELECTOR, "input[data-testid=create-board-title-input]").send_keys(board_name)
     
-    @allure.step("Press button 'Create'")
+    @allure.step("UI. Press button 'Create' via")
     def click_create_button(self):
         # self.__driver.execute_script("window.scrollBy(0, 400);")
         button = WebDriverWait(self.__driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[data-testid=create-board-submit-button]")))
@@ -68,19 +69,19 @@ class MainPage:
         button.click()
         WebDriverWait(self.__driver, 20).until(EC.url_contains("https://trello.com/b/"))
         
-    @allure.step("Crerate new list {list_name}")
+    @allure.step("UI. Crerate new list {list_name}")
     def create_list(self, list_name: str):
         WebDriverWait(self.__driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.frrHNIWnTojsww.CSwccJ0PrMROzz.bxgKMAm3lq5BpA.SEj5vUdI3VvxDc"))).click()
         WebDriverWait(self.__driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea[class=oe8RymzptORQ7h]"))).send_keys(list_name)
         WebDriverWait(self.__driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.bxgKMAm3lq5BpA.SdamsUKjxSBwGb.SEj5vUdI3VvxDc"))).click()
         
-    @allure.step("Create new card {card_name}")
+    @allure.step("UI. Create new card {card_name}")
     def create_card(self, card_name: str):
         WebDriverWait(self.__driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.O9vivwyDxMqo3q.bxgKMAm3lq5BpA.iUcMblFAuq9LKn.PnEv2xIWy3eSui.SEj5vUdI3VvxDc"))).click()
         WebDriverWait(self.__driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea[class=qJv26NWQGVKzI9]"))).send_keys(card_name)
         WebDriverWait(self.__driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.bxgKMAm3lq5BpA.SdamsUKjxSBwGb.SEj5vUdI3VvxDc"))).click()
         
-    @allure.step("Drag and drop card to another list")
+    @allure.step("UI. Drag and drop card to another list")
     def drag_and_drop_onto_element(self):
         draggable = self.__driver.find_element(By.CSS_SELECTOR, "div.KWQlnMvysRK4fI.ui-droppable")
         droppable = self.__driver.find_element(By.CSS_SELECTOR, "ol[class=RD2CmKQFZKidd6]")
@@ -88,7 +89,7 @@ class MainPage:
             .drag_and_drop(draggable, droppable)\
             .perform()
     
-    @allure.step("Update title card - new name {new_card_name}")
+    @allure.step("UI. Update title card - new name {new_card_name}")
     def update_card(self, new_card_name: str):
         WebDriverWait(self.__driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[class=amUfYqLTZOvGsn]"))).click()
         WebDriverWait(self.__driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "textarea.mod-card-back-title.js-card-detail-title-input"))).clear()
